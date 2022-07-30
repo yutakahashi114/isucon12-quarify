f0c459a121787d23b18b8becd4b00e379ec160c1
takahashiyu114 <takayou1208@gmail.com>
2022-07-30 20:37:46 +0900

    visit_history_2

--- go/isuports.go
+++ go/isuports.go
@@ -644,7 +644,8 @@ func billingReportByCompetition(ctx context.Context, tenantDB dbOrTx, tenantID i
 	if err := adminDB.SelectContext(
 		ctx,
 		&vhs,
-		"SELECT player_id, MIN(created_at) AS min_created_at FROM visit_history WHERE tenant_id = ? AND competition_id = ? GROUP BY player_id",
+		// "SELECT player_id, MIN(created_at) AS min_created_at FROM visit_history WHERE tenant_id = ? AND competition_id = ? GROUP BY player_id",
+		"SELECT player_id, created_at AS min_created_at FROM visit_history_2 WHERE tenant_id = ? AND competition_id = ?",
 		tenantID,
 		comp.ID,
 	); err != nil && err != sql.ErrNoRows {
@@ -1652,36 +1653,34 @@ func competitionRankingHandler(c echo.Context) error {
 		return fmt.Errorf("error Select tenant: id=%d, %w", v.tenantID, err)
 	}
 
-	/*
-		vh := VisitHistoryRow{}
-		if err := adminDB.GetContext(
-			ctx,
-			&vh,
-			"SELECT * FROM visit_history WHERE tenant_id = ? AND competition_id = ? AND player_id = ?",
-			tenant.ID, competitionID, v.playerID,
-		); err != nil {
-			if _, err := adminDB.ExecContext(
-				ctx,
-				"INSERT INTO visit_history (player_id, tenant_id, competition_id, created_at, updated_at) VALUES (?, ?, ?, ?, ?)",
-				v.playerID, tenant.ID, competitionID, now, now,
-			); err != nil {
-				return fmt.Errorf(
-					"error Insert visit_history: playerID=%s, tenantID=%d, competitionID=%s, createdAt=%d, updatedAt=%d, %w",
-					v.playerID, tenant.ID, competitionID, now, now, err,
-				)
-			}
-		}
-	*/
-	if _, err := adminDB.ExecContext(
+	vh := VisitHistoryRow{}
+	if err := adminDB.GetContext(
 		ctx,
-		"INSERT INTO visit_history (player_id, tenant_id, competition_id, created_at, updated_at) VALUES (?, ?, ?, ?, ?)",
-		v.playerID, tenant.ID, competitionID, now, now,
+		&vh,
+		"SELECT * FROM visit_history_2 WHERE tenant_id = ? AND competition_id = ? AND player_id = ?",
+		tenant.ID, competitionID, v.playerID,
 	); err != nil {
-		return fmt.Errorf(
-			"error Insert visit_history: playerID=%s, tenantID=%d, competitionID=%s, createdAt=%d, updatedAt=%d, %w",
-			v.playerID, tenant.ID, competitionID, now, now, err,
-		)
+		if _, err := adminDB.ExecContext(
+			ctx,
+			"INSERT INTO visit_history_2 (player_id, tenant_id, competition_id, created_at, updated_at) VALUES (?, ?, ?, ?, ?)",
+			v.playerID, tenant.ID, competitionID, now, now,
+		); err != nil {
+			return fmt.Errorf(
+				"error Insert visit_history: playerID=%s, tenantID=%d, competitionID=%s, createdAt=%d, updatedAt=%d, %w",
+				v.playerID, tenant.ID, competitionID, now, now, err,
+			)
+		}
 	}
+	// if _, err := adminDB.ExecContext(
+	// 	ctx,
+	// 	"INSERT INTO visit_history (player_id, tenant_id, competition_id, created_at, updated_at) VALUES (?, ?, ?, ?, ?)",
+	// 	v.playerID, tenant.ID, competitionID, now, now,
+	// ); err != nil {
+	// 	return fmt.Errorf(
+	// 		"error Insert visit_history: playerID=%s, tenantID=%d, competitionID=%s, createdAt=%d, updatedAt=%d, %w",
+	// 		v.playerID, tenant.ID, competitionID, now, now, err,
+	// 	)
+	// }
 
 	var rankAfter int64
 	rankAfterStr := c.QueryParam("rank_after")
--- sql/init.sql
+++ sql/init.sql
@@ -1,4 +1,18 @@
 DELETE FROM tenant WHERE id > 100;
 DELETE FROM visit_history WHERE created_at >= '1654041600';
+DELETE FROM visit_history_2 WHERE created_at >= '1654041600';
 UPDATE id_generator SET id=2678400000 WHERE stub='a';
 ALTER TABLE id_generator AUTO_INCREMENT=2678400000;
+
+-- DROP TABLE IF EXISTS `visit_history_2`;
+
+-- CREATE TABLE `visit_history_2` (
+--   `player_id` VARCHAR(255) NOT NULL,
+--   `tenant_id` BIGINT UNSIGNED NOT NULL,
+--   `competition_id` VARCHAR(255) NOT NULL,
+--   `created_at` BIGINT NOT NULL,
+--   `updated_at` BIGINT NOT NULL,
+--   PRIMARY KEY (`tenant_id`, `competition_id`, `player_id`)
+-- ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
+
+-- INSERT INTO visit_history_2 SELECT player_id, tenant_id, competition_id, min(created_at) AS created_at, min(created_at) AS updated_at FROM visit_history GROUP BY `tenant_id`, `competition_id`, `player_id`
